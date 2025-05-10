5b9a95df6851d9e1a75dedcf23a9d73d3d29b905
Zidan Syauqi Rahman <zidanrahman674@gmail.com>
2025-05-10 13:24:05 +0700

    first commit

---
+++ array/tipe_data_array.js
@@ -0,0 +1,3 @@
+let array_kosong = []
+let array_nama=["Zidan", "syauqi", "rahman"]
+console.log(array_nama)
---
+++ latihan/indexString_stringMethods.js
@@ -0,0 +1,13 @@
+const text = " halo dunia! "
+console.log("karakter pertama : " + text[0])
+console.log("karakter ke 4" + text[4])
+console.log("karakter terakhir :", text.charAt(text.length - 2))
+console.log("text 15 js ", text[15])
+console.log("hasil charat 12", text.charAt(12))
+
+//MetodeString
+console.log(text.toUpperCase())
+console.log(text.length)
+console.log(text.replace("halo", "hai"))
+console.log(text.indexOf("a"))
+console.log(text.trim())
