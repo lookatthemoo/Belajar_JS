1bb64defbbc5d16dc59c557a93f32241aa5dfabe
Zidan Syauqi Rahman <zidanrahman674@gmail.com>
2025-05-11 12:27:30 +0700

    perubahan baru

---
+++ typeof/typeof.js
@@ -0,0 +1,20 @@
+let angka = 42;
+console.log(typeof angka);
+
+let teks = "Halo";
+console.log(typeof teks);
+
+let benar = true;
+console.log(typeof benar);
+
+let tidakAda = null;
+console.log(typeof tidakAda);
+
+let belumDidefinisikan;
+console.log(typeof belumDidefinisikan);
+
+let objek = {};
+console.log(typeof objek);
+
+let fungsi = function() {};
+console.log(typeof fungsi);
